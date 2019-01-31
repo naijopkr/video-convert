cbd3eec0a16ab3d88a2e3a058ed65055dc8837c7
Ariel Barcellos <barcellos.ariel@gmail.com>
2019-01-30 20:06:15 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/index.js'
+  ],
+  target: 'node',
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['css-loader'],
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    port: 4172
+  }
+}
