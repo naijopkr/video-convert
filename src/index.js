aeba5a494217af48644f7ff19c32a6fff5f369b5
Ariel Barcellos <barcellos.ariel@gmail.com>
2019-01-30 20:21:15 -0500

    added react components and plugins

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
+
+import ConvertScreen from './screens/ConvertScreen'
+import VideoSelectScreen from './screens/VideoSelectScreen'
 
 import './index.css'
 
+const app = (
+  <Provider store={store}>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/convert" component={ConvertScreen} />
+          <Route path="/" component={VideoSelectScreen} />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>
+)
+
 ReactDOM.render(
-  <div>Hello World!</div>, 
+  app, 
   document.querySelector('#root')
 )
